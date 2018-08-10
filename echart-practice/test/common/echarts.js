d7643554bb2653a0b43c2282834005d2af456b29
rujptw <2609274343@qq.com>
2018-08-11 01:33:14 +0800

    echarts练习

--- echart-practice/test/common/echarts.js
+++ echart-practice/test/common/echarts.js
@@ -5,8 +5,18 @@ let echarts = require('echarts/lib/echarts')
 //引入需要的组件
 require('echarts/lib/chart/line')
 require('echarts/lib/chart/bar')
+require('echarts/lib/chart/pie')
+require('echarts/lib/chart/gauge')
+
 require('echarts/lib/component/tooltip')
 require('echarts/lib/component/title')
 require('echarts/lib/component/legend')
+require('echarts/lib/component/toolbox')
+require('echarts/lib/component/markLine')
+require('echarts/lib/component/markPoint')
+require('echarts/lib/component/dataZoom')
+
+
+
 
 export default echarts
