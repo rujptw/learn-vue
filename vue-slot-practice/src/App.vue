689202073dab7a09f654d0cd279de5ca87de9691
rujptw <2609274343@qq.com>
2019-01-11 11:00:06 +0800

    增加样式绑定说明和v-for和 v-if使用注意事项

--- vue-slot-practice/src/App.vue
+++ vue-slot-practice/src/App.vue
@@ -1,36 +1,37 @@
 <template>
   <div id="app">
     <hello-world>
-      <template  slot="test1" slot-scope="datas">
+      <template slot-scope="datas">
         <ul>
-          <li v-for="(item,index) in datas.testdata" :key="index + 'tree'">{{item}}</li>
+          <li v-for="(item,index) in datas.testdata" :key="index + 'tree'">
+            {{ item }}
+          </li>
         </ul>
       </template>
       <p slot="test2">we dont known enough information about this machine</p>
-      <p> i am coming</p>
+      <p>i am coming</p>
     </hello-world>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+  import HelloWorld from "./components/HelloWorld";
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
-};
+  export default {
+    name: "App",
+    components: {
+      HelloWorld
+    }
+  };
 </script>
 
 <style lang="stylus" scoped>
- #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #1983ee0c;
-  margin-top: 60px;
-}
-
+   #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #1983ee0c;
+    margin-top: 60px;
+  }
 </style>
--- vue-slot-practice/src/components/HelloWorld.vue
+++ vue-slot-practice/src/components/HelloWorld.vue
@@ -1,42 +1,38 @@
 <template>
-  <div class='hello'>
-      <p class="title">作用域插槽也可以加上name属性,数据由子组件提供</p>
-      <slot name="test1" :testdata="testdata"></slot> 
-      <hr/>
-      <p class="title">默认插槽:父组件不提供内容,slot自己提供</p>
+  <div class="hello">
+    <p class="title">作用域插槽也可以加上name属性,数据由子组件提供</p>
+    <slot :testdata="testdata"></slot>
+    <hr />
+    <!-- <p class="title">默认插槽:父组件不提供内容,slot自己提供</p>
       <slot>默认插槽</slot>
-      <hr/>
-      <p class="title">普通插槽:父组件提供什么内容，子组件就显示什么内容，默认插槽会失效，由父组件的内容代替其中的内容,上面的一个就是被替代了</p>
-      <slot></slot>
-      <hr/>
-      <p class="title">命名插槽：名字要一 一对应，才能对应的渲染</p>
-      <slot name="test2"></slot>
+      <hr/> -->
+    <!-- <p class="title">普通插槽:父组件提供什么内容，子组件就显示什么内容，默认插槽会失效，由父组件的内容代替其中的内容,上面的一个就是被替代了</p>
+      <slot></slot> -->
+    <hr />
+    <!-- <p class="title">命名插槽：名字要一 一对应，才能对应的渲染</p>
+      <slot name="test2"></slot> -->
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
+  export default {
+    name: "HelloWorld",
 
-  data () {
-    return {
-        testdata: [1,2,3,4,5]
-    }
-  },
+    data() {
+      return {
+        testdata: [1, 2, 3, 4, 5]
+      };
+    },
 
-props:{
-},
-mounted(){
-},
-computed:{
-},
-methods: {
-}
-}
+    props: {},
+    mounted() {},
+    computed: {},
+    methods: {}
+  };
 </script>
 
-<style lang='stylus' scoped>
+<style lang="stylus" scoped>
   .title{
-    color orange 
+    color orange
   }
 </style>
